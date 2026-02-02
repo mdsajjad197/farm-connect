fab95b7716fc7ef4d3f6a43ba8bbc8eea417ca50
mdsajjad197 <sajjadsaffan634@gmail.com>
2026-02-02 17:07:02 +0530

    server modify

--- backend/server.js
+++ backend/server.js
@@ -25,9 +25,7 @@ app.use("/api/user", userRoutes);
 app.use("/api/consumer", consumerRoutes);
 app.use("/api/admin", adminRoutes);
 app.use("/api/feedback", feedbackRoutes);
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+
 
 app.listen(process.env.PORT, () => {
   console.log(`🚀 Server running on port ${process.env.PORT}`);
