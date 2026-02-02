a6e795d77b5eaa15dc7ee440bc819204cf398ef2
mdsajjad197 <sajjadsaffan634@gmail.com>
2026-02-02 17:02:07 +0530

    axios modify

--- farmconnect-frontend/src/api/axios.js
+++ farmconnect-frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://farm-connect-sand.vercel.app",
 });
 
 api.interceptors.request.use((req) => {
